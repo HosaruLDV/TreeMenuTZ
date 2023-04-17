ea38fcd6b5ed8bada82fd88a473ed140e6c32f97
Kirill <kirill.potapenkov@mail.ru>
2023-04-17 21:15:31 +0300

    FirstCommit

---
+++ menu/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+
+from menu.apps import MenuConfig
+from menu.views import IndexPageView
+
+
+app_name = MenuConfig.name
+
+urlpatterns = [
+    path('menu/', IndexPageView.as_view(), name='index')
+]
